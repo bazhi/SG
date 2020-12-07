4e7f76d15340038572a6fbd38a222b286a12d2eb
bazhicc <bazhicc@163.com>
2020-12-07 20:49:28 +0800

    修改名字定义

--- Source/SG/Character/GSInputName.h
+++ Source/SG/Character/GSInputName.h
@@ -1,13 +1,12 @@
 #pragma once
-#include <string>
 
 #include "UObject/NameTypes.h"
 #include "CoreMinimal.h"
 
 namespace GSInputName
 {
-    static class FName MoveForwardBackward = "MoveForward/Backwards";
-    static class FName MoveRightLeft = "MoveRight/Left";
-    static class FName LookUpDown = "LookUp/Down";
-    static class FName LookLeftRight = "LookLeft/Right";
+    const FName MoveForwardBackward = "MoveForward/Backwards";
+    const FName MoveRightLeft = "MoveRight/Left";
+    const FName LookUpDown = "LookUp/Down";
+    const FName LookLeftRight = "LookLeft/Right";
 };
